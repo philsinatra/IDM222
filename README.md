3f6e353d0852b43c572792d4e9faab981b9a865a
Phil Sinatra <code@philsinatra.com>
2016-11-13 14:55:14 -0500

    🔧  Add make Script

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# IDM 222
+# IDM 222: Web Authoring II
 
-## Web Authoring II
+## Instructor Setup
+
+Setup additional teaching assets:
+
+```bash
+cd ./bin/
+./make.sh
+```
--- bin/make.sh
+++ bin/make.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+cd ${0%/*}
+
+echo "Downloading instructor materials"
+curl http://digm.drexel.edu/crs/IDM222/cdn/IDM222-W08.md -o ../instructor_materials/IDM222-W08.md
+echo "Done"
+echo ""
+echo "Downloading image assets"
+curl -LOk http://digm.drexel.edu/crs/IDM222/cdn/week4/images.zip
+echo "Done"
+echo ""
+echo "Extracting assets"
+mv images.zip ../examples/week4/pictures/.
+cd ../examples/week4/pictures/
+unzip images.zip
+rm images.zip
+echo "Done"
+cd -
